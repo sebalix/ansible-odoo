31852e7ed1a902ac2e53f5b08294ce7b2b1c2e8f
sebalix <seb@usr-src.org>
2018-02-26 17:45:19 +0100

    [WIP] Test the role with Python >= 3.5

--- tests/install_test_env.sh
+++ tests/install_test_env.sh
@@ -4,11 +4,11 @@ VENV=/opt/ansible-venv
 GET_PIP_URL="https://bootstrap.pypa.io/get-pip.py"
 # Install system dependencies
 apt-get update -qq
-apt-get install -qq python-virtualenv python-apt python-pip python-dev lsb-release wget ca-certificates
+apt-get install -qq python-virtualenv python3-apt python3-psycopg2 python3-pip python3-dev lsb-release wget ca-certificates
 # Install Ansible in a virtual Python environment
-virtualenv $VENV
+virtualenv $VENV --python=/usr/bin/python3
 wget $GET_PIP_URL -O $VENV/get-pip.py
-$VENV/bin/python $VENV/get-pip.py
-$VENV/bin/pip install "ansible>=$ANSIBLE_VERSION"
+$VENV/bin/python3 $VENV/get-pip.py
+$VENV/bin/pip3 install "ansible>=$ANSIBLE_VERSION"
 # Install PostgreSQL
 apt-get install -qq postgresql postgresql-contrib
